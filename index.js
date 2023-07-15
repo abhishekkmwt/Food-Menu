9df5a4709c0123f11d5e0f8f076d3eff95e204e2
abhishekkmwt <kumawat.abhishek167@gmail.com>
2023-07-15 23:58:37 +0530

    updated

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const url="https://raw.githubusercontent.com/saksham-accio/f2_contest_3/main/food.json"
-
 async function fetchData(){
     const response = await fetch(url);
     const result=await response.json();
     getMenu(result);
+    dataItems(result);
+    
 }
 
 fetchData();
+
+const items=[];
+function dataItems(data){
+    data.forEach((element)=>{
+        items.push(element);
+    })
+}
+console.log(items);
 
 const conatiner= document.getElementsByClassName("container")[0];
 
@@ -30,3 +39,26 @@ function getMenu(data){
     });
     
 }
+
+const orderTaken=[];
+
+function takeOrder(items){
+    return new Promise((resolve)=>{
+        setTimeout(function(){
+            resolve(items);
+        },2500);
+    })
+}
+
+
+takeOrder(items).then((data)=>{
+    for(let x=0;x<3;x++){
+        let y =Math.floor(Math.random() * data.length);
+        orderTaken.push(data[y]);
+    }
+    console.log(orderTaken);
+}).catch((error)=>{
+    console.log("error.message");
+})
+
+
